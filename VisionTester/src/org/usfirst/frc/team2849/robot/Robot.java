baf604cce721b78814311210cafb9b511e8f1a17
Acer <teamursamajor@gmail.com>
2017-01-10 17:49:03 -0500

    Consolidated Vision into one Robot.java file in VisionTester. Camera feed and OpenCV work with little latency.

--- VisionTester/src/org/usfirst/frc/team2849/robot/Robot.java
+++ VisionTester/src/org/usfirst/frc/team2849/robot/Robot.java
@@ -5,6 +5,7 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 /**
@@ -24,6 +25,8 @@ public class Robot extends IterativeRobot {
     	System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
 		System.out.println("mat = " + mat.dump());
+		
+		CameraServer.getInstance().startAutomaticCapture();
     }
     
 	/**
