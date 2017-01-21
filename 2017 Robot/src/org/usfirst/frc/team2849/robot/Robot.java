3ae462ee85a8932f3342a81ecfa839fe5d04bd6d
Acer <teamursamajor@gmail.com>
2017-01-21 09:17:42 -0500

    Added shooter code, added joysticks to visiontester, and added getAxis methods to joystick.

--- 2017 Robot/src/org/usfirst/frc/team2849/robot/Robot.java
+++ 2017 Robot/src/org/usfirst/frc/team2849/robot/Robot.java
@@ -2,6 +2,8 @@
 package org.usfirst.frc.team2849.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -12,58 +14,71 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  */
 public class Robot extends IterativeRobot {
 
-	public static XboxController joy = new XboxController(0);
-	
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
-    	
-    }
-    
-    
-    
+	Talon t1 = new Talon(0); // front left
+	Talon t2 = new Talon(1); // rear left
+	Talon t3 = new Talon(2); // front right
+	Talon t4 = new Talon(3); // rear right
+	RobotDrive drive = new RobotDrive(t1, t2, t3, t4);
+	public static LogitechFlightStick joy = new LogitechFlightStick(0);
+
 	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
-	 * If using the SendableChooser make sure to add them to the chooser code above as well.
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
 	 */
-    public void autonomousInit() {
-    	
-    }
+	public void robotInit() {
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-    	
-    }
+	}
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-    	
-     //   drive.mecanumDrive_Cartesian(joy1.getRawAxis(XboxController.AXIS_LEFTSTICK_X), joy1.getRawAxis(XboxController.AXIS_LEFTSTICK_Y), joy1.getRawAxis(XboxController.AXIS_RIGHTSTICK_X), 0);
-        
-//        Drive.mecanumDrive(joy.getAxisGreaterThan(XboxController.AXIS_LEFTSTICK_X, .1), joy.getAxisGreaterThan(XboxController.AXIS_LEFTSTICK_Y, .1), joy.getAxisGreaterThan(XboxController.AXIS_RIGHTSTICK_X, .1));
-        Shooter.shoot(joy.getAxisGreaterThan(XboxController.AXIS_RIGHTTRIGGER, .1));
-        Drive.mecanumDrive(joy.getAxisGreaterThan(XboxController.AXIS_LEFTSTICK_X, .1), joy.getAxisGreaterThan(XboxController.AXIS_LEFTSTICK_Y, .1), joy.getAxisGreaterThan(XboxController.AXIS_RIGHTSTICK_X, .1));
-        
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-    
-    }
-    public void disabledPeriodic(){
-    
-    }
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the
+	 * switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	public void autonomousInit() {
+
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+
+		drive.mecanumDrive_Cartesian(joy.getAxisGreaterThan(LogitechFlightStick.AXIS_TILT_X, .1),
+				joy.getAxisGreaterThan(LogitechFlightStick.AXIS_TILT_Y, .1),
+				joy.getAxisGreaterThan(-LogitechFlightStick.AXIS_ROTATE_Z, .1), 0);
+
+		// Drive.mecanumDrive(joy.getAxisGreaterThan(XboxController.AXIS_LEFTSTICK_X,
+		// .1), joy.getAxisGreaterThan(XboxController.AXIS_LEFTSTICK_Y, .1),
+		// joy.getAxisGreaterThan(XboxController.AXIS_RIGHTSTICK_X, .1));
+		// Shooter.shoot(joy.getAxisGreaterThan(XboxController.AXIS_RIGHTTRIGGER,
+		// .1));
+		// Drive.mecanumDrive(joy.getAxisGreaterThan(XboxController.AXIS_LEFTSTICK_X,
+		// .1), joy.getAxisGreaterThan(XboxController.AXIS_LEFTSTICK_Y, .1),
+		// joy.getAxisGreaterThan(XboxController.AXIS_RIGHTSTICK_X, .1));
+
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+
+	}
+
+	public void disabledPeriodic() {
+
+	}
 }
