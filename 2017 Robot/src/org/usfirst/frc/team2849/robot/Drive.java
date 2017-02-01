7b2d9a7c56c7614f13f3617b52085ca5edf50c5c
Nick Hestnes <kingkrazy@verizon.net>
2017-01-31 19:36:43 -0500

    Fixed and beautified the Drive.java class so that mecanumDrive() actually works the way we want it to. driveDirection(), driveAngle(), and mecDriveDistance() methods currently NOT WORKING. Will be fixed on next commit.

--- 2017 Robot/src/org/usfirst/frc/team2849/robot/Drive.java
+++ 2017 Robot/src/org/usfirst/frc/team2849/robot/Drive.java
@@ -2,26 +2,22 @@ package org.usfirst.frc.team2849.robot;
 
 import com.kauailabs.navx.frc.AHRS;
 
-import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.hal.FRCNetComm.tInstances;
-import edu.wpi.first.wpilibj.hal.FRCNetComm.tResourceType;
-import edu.wpi.first.wpilibj.hal.HAL;
+import edu.wpi.first.wpilibj.Spark;
 
 public class Drive implements Runnable {
 
 	/**
-	 * can't have talons and RobotDrive inside another robot drive
+	 * can't have Sparks and RobotDrive inside another robot drive
 	 * you would have to dig in to find out why, but that's the issue
 	 * 
 	 * - other charlie
 	 */
-//	private static Talon topleft = new Talon(0);
-//	private static Talon topright = new Talon(1);
-//	private static Talon bottomleft = new Talon(2);
-//	private static Talon bottomright = new Talon(3);
+//	private static Spark topleft = new Spark(0);
+//	private static Spark topright = new Spark(1);
+//	private static Spark bottomleft = new Spark(2);
+//	private static Spark bottomright = new Spark(3);
 	private static AHRS ahrs = new AHRS(SPI.Port.kMXP);
 //	private static RobotDrive drive = new RobotDrive(topleft, topright, bottomleft, bottomright);
 	private static double distance;
@@ -30,20 +26,83 @@ public class Drive implements Runnable {
 	private static Boolean bool = false;
 	private static EndCondition ending = null;
 	private static Thread driveRunner = null;
-	
 	private final double RL_SCALE = .05;
 	private final double RL_THRESH = .25;
+	
+	private Spark frontLeftMotor1;
+	private Spark frontLeftMotor2;
+	private Spark frontRightMotor1;
+	private Spark frontRightMotor2;
+	private Spark backLeftMotor1;
+	private Spark backLeftMotor2;
+	private Spark backRightMotor1;
+	private Spark backRightMotor2;
+	private int numMotors;
+	
 
 //	private Drive(double distance, double angle) {
 //		Drive.distance = distance;
 //		Drive.angle = angle;
 //	}
-	
-	public Drive(SpeedController t1, SpeedController t2, SpeedController t3, SpeedController t4) {
+	public Drive(int t1, int t3, 
+				 int t2, int t4) {
+		
+		frontLeftMotor1 = new Spark(t1);
+		frontRightMotor1 = new Spark(t2);
+		backLeftMotor1 = new Spark(t3);
+		backRightMotor1 = new Spark(t4);
+		numMotors = 4;
 	}
 	
-	public Drive() {
+	
+	public Drive(int t1, int t3, 
+				 int t2, int t4,
+				 
+				 int t5, int t7, 
+				 int t6, int t8) {
+		
+		frontLeftMotor1 = new Spark(t1);
+		frontLeftMotor2 = new Spark(t2);
+		frontRightMotor1 = new Spark(t3);
+		frontRightMotor2 = new Spark(t4);
+		backLeftMotor1 = new Spark(t5);
+		backLeftMotor2 = new Spark(t6);
+		backRightMotor1 = new Spark(t7);
+		backRightMotor2 = new Spark(t8);
+		numMotors = 8;
+		
 	}
+	
+	
+	 /**
+	   * Normalize all wheel speeds if the magnitude of any wheel is greater than 1.0.
+	   */
+	 protected void normalize(double[] wheelSpeeds) {
+		    double maxMagnitude = Math.abs(wheelSpeeds[0]);
+		    for (int i = 1; i < numMotors; i++) {
+		      double temp = Math.abs(wheelSpeeds[i]);
+		      if (maxMagnitude < temp) {
+		        maxMagnitude = temp;
+		      }
+		    }
+		    if (maxMagnitude > 1.0) {
+		      for (int i = 0; i < numMotors; i++) {
+		        wheelSpeeds[i] = wheelSpeeds[i] / maxMagnitude;
+		      }
+		    }
+		  }
+	 /**
+	   * Rotate a vector in Cartesian space.
+	   */
+	  @SuppressWarnings("ParameterName")
+	  protected double[] rotateVector(double x, double y, double angle) {
+	    double cosA = Math.cos(angle * (Math.PI / 180.0));
+	    double sinA = Math.sin(angle * (Math.PI / 180.0));
+	    double[] out = new double[2];
+	    out[0] = x * cosA - y * sinA;
+	    out[1] = x * sinA + y * cosA;
+	    return out;
+	  }
 
 	/**
 	 * This will drive the robot in omnidirectional holonomic drive
@@ -56,21 +115,68 @@ public class Drive implements Runnable {
 	 *            The rotation of the joystick.
 	 * 
 	 */
-	public static void mecanumDrive(double xaxis, double yaxis, double raxis) {
+	public void mecanumDrive(double xaxis, double yaxis, double raxis, double gyroAngle) {
 
-		double r = Math.hypot(xaxis, yaxis);
-		double robotAngle = Math.atan2(yaxis, xaxis) - Math.PI / 4;
-		double cosu = Math.cos(robotAngle);
-		double sinu = Math.sin(robotAngle);
-		final double v1 = r * cosu + raxis;
-		final double v2 = r * sinu - raxis;
-		final double v3 = r * sinu + raxis;
-		final double v4 = r * cosu - raxis;
+		    double xIn = xaxis;
+		    double yIn = yaxis;
+		    // Negate y for the joystick.
+		    yIn = -yIn;
+		    // Compenstate for gyro angle.
+		    double[] rotated = rotateVector(xIn, yIn, gyroAngle);
+		    xIn = rotated[0];
+		    yIn = rotated[1];
+
+		    if(numMotors == 4){
+			    double[] wheelSpeeds = new double[numMotors];
+			    wheelSpeeds[0] = xIn + yIn + raxis;		    
+			    wheelSpeeds[1] = -xIn + yIn - raxis;		    
+			    wheelSpeeds[2] = -xIn + yIn + raxis;	    
+			    wheelSpeeds[3] = xIn + yIn - raxis;
+			    
+			    normalize(wheelSpeeds);
+			    frontLeftMotor1.set(wheelSpeeds[0]);			    
+			    frontRightMotor1.set(wheelSpeeds[1]);		    
+			    backLeftMotor1.set(wheelSpeeds[2]);		    
+			    backRightMotor1.set(wheelSpeeds[3]);
+			    
+		    }else{
+		    	double[] wheelSpeeds = new double[numMotors];
+			    wheelSpeeds[0] = xIn + yIn + raxis;	
+			    wheelSpeeds[1] = xIn + yIn + raxis;	
+			    wheelSpeeds[2] = -xIn + yIn - raxis;
+			    wheelSpeeds[3] = -xIn + yIn - raxis;
+			    wheelSpeeds[4] = -xIn + yIn + raxis;
+			    wheelSpeeds[5] = -xIn + yIn + raxis;
+			    wheelSpeeds[6] = xIn + yIn - raxis;
+			    wheelSpeeds[7] = xIn + yIn - raxis;
+			    
+			    normalize(wheelSpeeds);
+			    
+			    frontLeftMotor1.set(wheelSpeeds[0]);
+			    frontLeftMotor2.set(wheelSpeeds[1]);
+			    frontRightMotor1.set(wheelSpeeds[2]);
+			    frontRightMotor2.set(wheelSpeeds[3]);
+			    backLeftMotor1.set(wheelSpeeds[4]);
+			    backLeftMotor2.set(wheelSpeeds[5]);
+			    backRightMotor1.set(wheelSpeeds[6]);
+			    backRightMotor2.set(wheelSpeeds[7]);
+		    }
+
+		  
+		  }
+//		double r = Math.hypot(xaxis, yaxis);
+//		double robotAngle = Math.atan2(yaxis, xaxis) - Math.PI / 4;
+//		double cosu = Math.cos(robotAngle);
+//		double sinu = Math.sin(robotAngle);
+//		final double v1 = r * cosu + raxis;
+//		final double v2 = r * sinu - raxis;
+//		final double v3 = r * sinu + raxis;
+//		final double v4 = r * cosu - raxis;
 //		topleft.set(v1);
 //		topright.set(v2);
 //		bottomleft.set(v3);
 //		bottomright.set(v4);
-	}
+	
 
 	/**
 	 * Drives the robot in a direction without a stop.
@@ -78,7 +184,7 @@ public class Drive implements Runnable {
 	 * @param angleDeg
 	 *            An angle measurement in radians.
 	 */
-	public static void driveDirection(double angleDeg) {
+	public void driveDirection(double angleDeg) {
 
 		double angleRad = angleDeg * (Math.PI / 180);
 		double cosu = Math.cos(angleRad);
@@ -102,7 +208,7 @@ public class Drive implements Runnable {
 	 * @param time
 	 *            A time measurement in milliseconds.
 	 */
-	public static void driveDirection(double angleDeg, int time) {
+	public void driveDirection(double angleDeg, int time) {
 
 		long timer = System.currentTimeMillis();
 		double angleRad = angleDeg * (Math.PI / 180);
@@ -131,7 +237,7 @@ public class Drive implements Runnable {
 //			drive.stopMotor();
 //		}
 	}
-	public static void mechDriveDistance(double distance, double angleDeg) { // in meters
+	public void mechDriveDistance(double distance, double angleDeg) { // in meters
 
 		double displacement = 0;
 		ahrs.resetDisplacement();
@@ -199,11 +305,11 @@ public class Drive implements Runnable {
 		}
 	}
 
-	public static void startDrive() {
-		driveRunner = new Thread(new Drive(), "drive");
-		driveRunner.start();
-
-	}
+//	public static void startDrive() {
+//		driveRunner = new Thread(new Drive(), "drive");
+//		driveRunner.start();
+//
+//	}
 	
 //	@SuppressWarnings("ParameterName")
 //	  public void mecanumDrive_Cartesian(double x, double y, double rotation, double gyroAngle) {
