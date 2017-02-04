9de81819dfd259256b4d87b47b668a4a75af172c
MSinnott <msinnott16@gmail.com>
2017-02-04 09:43:25 -0500

    Added multiple modes for autonomous.

--- 2017 Robot/src/org/usfirst/frc/team2849/robot/Autonomous.java
+++ 2017 Robot/src/org/usfirst/frc/team2849/robot/Autonomous.java
@@ -1,61 +1,89 @@
 package org.usfirst.frc.team2849.robot;
 
-public class Autonomous implements Runnable {
+import java.util.List;
 
-	private static AutoMode mode;
+public class Autonomous implements Runnable {
+	
+	private static Drive drive;
+	private static List<AutoMode> mode;
 	private static Thread autoRunner = null;
 	private static EndCondition ending = null;
-	private static Boolean bool = false;
-
+	private static Boolean threadOneUse = false;
+	private static StartPosition position = null;
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		switch (mode) {
-		case CROSS:
-			cross();
-			break;
-		case SHOOT:
-			shoot();
-			break;
-		case GEAR:
-			gear();
-			break;
-		case NONE:
-		default:
-			// do nothing
-			break;
+		AutoMode previousMode;
+		AutoMode currentMode;
+		if (mode == null || mode.size() == 0) {
+			return;
 		}
-		synchronized (bool) {
-			bool = false;
+		previousMode = mode.get(0);
+		for (int i = 0; i<mode.size(); i++) {
+			currentMode = mode.get(i);
+			switch (currentMode) {
+			case CROSS:
+				cross(previousMode);
+				break;
+			case SHOOT:
+				shoot(previousMode);
+				break;
+			case GEAR:
+				gear(previousMode);
+				break;
+			case NONE:
+			default:
+				// do nothing
+				break;
+			}
+			previousMode = currentMode;
+		}
+		synchronized (threadOneUse) {
+			threadOneUse = false;
 		}
 	}
 
-	public void cross() {
+	public void cross(AutoMode previousMode) {
 		while (!ending.done()) {
 			//Drive.drive(3, 0);
 		}
 	}
 
-	public void shoot() {
-
+	public void shoot(AutoMode previousMode) {
+		
 	}
 
-	public void gear() {
-
+	public void gear(AutoMode previousMode) {
+		if (previousMode == AutoMode.GEAR) {
+			if (position == StartPosition.LEFT) {
+				Drive.driveDirection(0, 1000);
+				Drive.driveDirection(90, 1000);
+			} else if (position == StartPosition.RIGHT) {
+				Drive.driveDirection(0, 1000);
+				Drive.driveDirection(-90, 1000);
+			} else if (position == StartPosition.CENTER) {
+				Drive.driveDirection(0, 1000);
+			}
+		} else if (previousMode == AutoMode.SHOOT){
+			Drive.driveDirection(180, 2000);
+		} else if (previousMode == AutoMode.CROSS) {
+			
+		}
 	}
 
-	private Autonomous(EndCondition ending, AutoMode mode) {
+	private Autonomous(EndCondition ending, List <AutoMode> mode, StartPosition position, Drive drive) {
 		Autonomous.ending = ending;
 		Autonomous.mode = mode;
+		Autonomous.position = position;
+		Autonomous.drive = drive;
 	}
 
-	public static void auto(EndCondition ending, AutoMode mode) {
-		synchronized (bool) {
-			if (bool)
+	public static void auto(EndCondition ending, List <AutoMode> mode, StartPosition position, Drive drive) {
+		synchronized (threadOneUse) {
+			if (threadOneUse)
 				return;
-			bool = true;
+			threadOneUse = true;
 		}
-		autoRunner = new Thread(new Autonomous(ending, mode), "auto");
+		autoRunner = new Thread(new Autonomous(ending, mode, position, drive), "auto");
 		autoRunner.start();
 	}
 }
